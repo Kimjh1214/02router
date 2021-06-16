367cf03408e5424cfc1cf74ea30ac3d93466deaf
Kimjh1214 <hzpotter1214@naver.com>
2021-06-16 15:20:00 +0900

    20210616

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="footer">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo!</p>
+    </footer>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
